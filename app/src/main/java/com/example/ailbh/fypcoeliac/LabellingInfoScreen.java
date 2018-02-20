21c08ee4054337581b4692e5a7358f4034673c98
Ailbhe <redmonai@tcd.ie>
2018-02-20 18:49:18 +0000

    some prettification

--- app/src/main/java/com/example/ailbh/fypcoeliac/LabellingInfoScreen.java
+++ app/src/main/java/com/example/ailbh/fypcoeliac/LabellingInfoScreen.java
@@ -3,14 +3,20 @@ package com.example.ailbh.fypcoeliac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class LabellingInfoScreen extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_labelling_info_screen);
+
+        textView = findViewById(R.id.body);
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
