14953a6e31258f3d43e7d6738a8ac683ddfb26e7
Ailbhe <redmonai@tcd.ie>
2018-02-27 18:05:20 +0000

    removed outdated classes and layouts

--- app/src/main/java/com/example/ailbh/fypcoeliac/MainActivity.java
+++ app/src/main/java/com/example/ailbh/fypcoeliac/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.ailbh.fypcoeliac;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private FloatingActionButton fab;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InformationScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        editText = (EditText) findViewById(R.id.searchBar1);
     }
+
+        //called when user taps the search button
+    public void sendSearch(View view)
+    {
+        Intent intent = new Intent(this, search_results.class);
+        String searchString = editText.getText().toString().toLowerCase();
+        System.out.println("first search string: " + searchString);
+        intent.putExtra("search-string", searchString);
+        startActivity(intent);
+    }
+
 }
+
