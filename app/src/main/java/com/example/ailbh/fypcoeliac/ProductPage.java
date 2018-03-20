33a39487df69378b9d44cf0f5467622a8ba7ef9b
Ailbhe <redmonai@tcd.ie>
2018-03-20 17:42:49 +0000

    add and remove from favourites on product pages working

--- app/src/main/java/com/example/ailbh/fypcoeliac/ProductPage.java
+++ app/src/main/java/com/example/ailbh/fypcoeliac/ProductPage.java
@@ -5,14 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +27,8 @@ public class ProductPage extends AppCompatActivity {
 
     private BottomNavigationView bottomNavView;
     private Button addFavButton;
+    private Button removeFavButton;
+    private Boolean favourite;
 
     private String name;
     private String brand;
@@ -40,6 +46,7 @@ public class ProductPage extends AppCompatActivity {
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseRef;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,14 +108,63 @@ public class ProductPage extends AppCompatActivity {
         productSize.setText(extras.getString("PRODUCT_SIZE"));
         key = (extras.getString("PRODUCT_KEY"));
 
+
         addFavButton = (Button) findViewById(R.id.addFavButton);
+        removeFavButton = (Button) findViewById(R.id.removeFavButton);
+//        addFavButton.setVisibility(View.VISIBLE);
+//        removeFavButton.setVisibility(View.INVISIBLE);
+
+        //determine if the product has been favourited or not
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseRef = mFirebaseDatabase.getReference().child("users").child(userID).child("favourites");
+        System.out.println(mDatabaseRef);
+        System.out.println(key);
+        System.out.println(mDatabaseRef.child(key));
+        mDatabaseRef.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {
+                    favourite = false;
+                    System.out.println("Favourite is false");
+                }
+                else {
+                    favourite = true;
+                    System.out.println("Favourite is true");
+                }
+                if (favourite)
+                {
+                    removeFavButton.setVisibility(View.VISIBLE);
+                    addFavButton.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    addFavButton.setVisibility(View.VISIBLE);
+                    removeFavButton.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("READ_FAILED", "Read failed");
+            }
+        });
+
         addFavButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mFirebaseDatabase = FirebaseDatabase.getInstance();
-                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                mDatabaseRef = mFirebaseDatabase.getReference().child("users").child(userID).child("favourites");
                 mDatabaseRef.child(key).setValue("true");
+                addFavButton.setVisibility(View.INVISIBLE);
+                removeFavButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        removeFavButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDatabaseRef.child(key).setValue("false");
+                removeFavButton.setVisibility(View.INVISIBLE);
+                addFavButton.setVisibility(View.VISIBLE);
             }
         });
     }
